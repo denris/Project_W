6e44b82a652ec54a904f2fff40d78f3e40f8ffc6
Denver Risser <denris97@gmail.com>
2018-07-12 11:45:25 -0400

    "Will Display People in DB, and can add more"

--- Wedding_Manager.py
+++ Wedding_Manager.py
@@ -27,38 +27,49 @@ class Application(ttk.Frame, Tk.Frame, Tk.PhotoImage):
         master.geometry("800x600")                           # Create instance      
         master.title("Python GUI")                 # Add a title 
         
+        =================================================================================================
         self.toolbar = Tk.Frame(master, bd=1, relief=Tk.RAISED)
         self.toolbar.pack(anchor="n", fill=Tk.X)
         #self.add_person_image = Tk.PhotoImage(file="add_person.png")
-        self.button = Tk.Button(self.toolbar, command=self.add_person)
-        self.button.pack(side="left")
+        self.add_person_image = Tk.PhotoImage(file='add_person.gif')
+        self.button = Tk.Button(self.toolbar, image=self.add_person_image, command=self.add_person)
+        self.button.pack(side="left", padx=2, pady=2)
         
-        self.separator1 = ttk.Separator(self.toolbar)
-        self.separator1.pack(side="left")
+        self.separator1 = ttk.Separator(self.toolbar, orient=Tk.VERTICAL)
+        self.separator1.pack(side="left", padx=2, fill=Tk.BOTH)
         
+        =================================================================================================
         self.tabControl = ttk.Notebook(master)          # Create Tab Control
         self.tabControl.pack(expand=1, fill="both")  # Pack to make visible
-        self.my_style = ttk.Style()
-        self.my_style.configure('My.TFrame', background='#fff')
+        self.style = ttk.Style()
+        self.style.configure(".", font=('Helvetica', 8), foreground="black")
+        self.style.configure("Treeview", foreground='black')
+        self.style.configure("Treeview.Heading", foreground='darkgreen')
 
-        self.tab1 = ttk.Frame(self.tabControl,  style='My.TFrame')
+        self.tab1 = ttk.Frame(self.tabControl)
         #tab1.grid(column=1,row=1,sticky='news')            # Create a tab 
-        self.tabControl.add(self.tab1, text='To do')      # Add the tab
+        self.tabControl.add(self.tab1, text='To do')
+        self.tab1.dataCols = ("First Name", "Last Name", "Status", "Relationship")
+        self.create_columns(self.tab1.dataCols, self.tab1)      # Add the tab
         
-        
+        =================================================================================================
         # Create Tab Control
         self.tab2 = ttk.Frame(self.tabControl)            # Create a tab 
         self.tabControl.add(self.tab2, text='Invited')      # Add the tab
         self.label2 = Tk.Label(self.tab2, text="Denver")
         self.label2.grid(row=1, column=0)
+        =================================================================================================
+        #Creating Methods
+        =================================================================================================
+    
+    def create_columns(self, dataCols, tab):
         # Adding the Columns for organization
-        self.columns_tab1 = ttk.Frame(self.tab1)
+        self.columns_tab1 = ttk.Frame(tab)
         self.columns_tab1.pack(side=Tk.TOP, fill=Tk.BOTH, expand=Tk.Y)
         
         # create the tree and scrollbars
-        self.dataCols = ('Name', 'Status', 'Phone')        
-        self.tree = ttk.Treeview(columns=self.dataCols, 
-                                 show = 'headings')
+        self.dataCols = dataCols       
+        self.tree = ttk.Treeview(columns=self.dataCols, show= 'headings')
         
         ysb = ttk.Scrollbar(orient=Tk.VERTICAL, command= self.tree.yview)
         xsb = ttk.Scrollbar(orient=Tk.HORIZONTAL, command= self.tree.xview)
@@ -76,10 +87,22 @@ class Application(ttk.Frame, Tk.Frame, Tk.PhotoImage):
 
         for n in self.dataCols:
             self.tree.heading(n, text=n.title())
-        self.load_data()
-    def load_data(self):
-        self.data = [self.person_label = Tk.Label(self.tab1, text=self.input)]
+        self.load_people_data()
+        
+    def load_people_data(self):
+        self.data = []
         # add data to the tree 
+        sql = "SELECT firstname, lastname, status, relationship FROM people"
+        self.res = cursor.execute(sql)
+        for self.row in self.res:
+            self.firstname = self.row[0]
+            self.lastname = self.row[1]
+            # self.street = self.row[2]
+            self.relationship = self.row[2]
+            self.status = self.row[3]
+            self.data.append([self.firstname, self.lastname, self.status, self.relationship])
+        
+        
         for item in self.data: 
             self.tree.insert('', 'end', values=item)
 
@@ -111,10 +134,17 @@ class Application(ttk.Frame, Tk.Frame, Tk.PhotoImage):
         sql = "INSERT INTO people (firstname) VALUES (?)"
         self.res = cursor.execute(sql, (self.input,))
         conn.commit()
-        # self.person_label = Tk.Label(self.tab1, text=self.input)
-        # self.data.append(self.person_label)
-        # self.load_data()
-
+        
+        sql = "SELECT firstname, lastname, status, relationship FROM people WHERE firstname=?"
+        self.res = cursor.execute(sql, (self.input,))
+        for self.row in self.res:
+            self.firstname = self.row[0]
+            self.lastname = self.row[1]
+            self.status = self.row[2]
+            self.relationship = self.row[3]
+            self.tree.insert('', 'end', values=[self.firstname, self.lastname, self.status, self.relationship])
+            
+        
 def main():     
         
     win = Tk.Tk()
@@ -128,7 +158,7 @@ main()
 cursor.close()
 del cursor
 conn.close()
-        
+
         
        
         
